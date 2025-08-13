6488281c5e08c49e1074d0d564491e8dd078db4e
THUNGERAMMOHAN <76059035+THUNGERAMMOHAN@users.noreply.github.com>
2025-08-13 20:17:38 +0530

    bulb

---
+++ bulb/dom2.js
@@ -0,0 +1,8 @@
+function switchon(){
+    document.getElementById("bulb").src="https://www.w3schools.com/html/pic_bulbon.gif";
+    document.body.style.backgroundColor="white";
+}
+function switchoff(){
+    document.getElementById("bulb").src="https://www.w3schools.com/html/pic_bulboff.gif";
+    document.body.style.backgroundColor="black"
+}   

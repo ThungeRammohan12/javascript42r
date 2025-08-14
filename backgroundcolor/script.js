983498f21dc74f8774197fafa9638fd8e2cbd066
THUNGERAMMOHAN <76059035+THUNGERAMMOHAN@users.noreply.github.com>
2025-08-14 14:12:51 +0530

    backgroundcolors

---
+++ backgroundcolor/script.js
@@ -0,0 +1,9 @@
+function red(){
+    document.body.style.backgroundColor="red";
+}
+function orange(){
+    document.body.style.backgroundColor="orange";
+}
+function blue(){
+    document.body.style.backgroundColor="blue";
+}
